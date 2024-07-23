1b82fbcc6d1e7adcb005adc8593ad33f51317413
safeiridwan <ahmadsafei.ridwan@gmail.com>
2024-07-23 14:58:01 +0700

    feat: health-check spring web

---
+++ src/main/java/com/aegis/configuration/WebConfiguration.java
@@ -0,0 +1,37 @@
+package com.aegis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebSecurity
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/health-check").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "PATCH", "OPTION")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
