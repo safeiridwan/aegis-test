4e9fc57aa629b4f85319b1c0f7dc41c892789910
safeiridwan <ahmadsafei.ridwan@gmail.com>
2024-07-24 09:08:06 +0700

    fix: email configuration

--- src/main/java/com/aegis/service/user/UserServiceImpl.java
+++ src/main/java/com/aegis/service/user/UserServiceImpl.java
@@ -45,7 +45,7 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
 
         EmailDetail mail = new EmailDetail(user.getEmail(), password);
-        mailUtil.sendMail(mail);
+        mailUtil.send(mail);
 
         return new ResponseEntity<>(new ResponseAPI(200, OK, null, new DetailUserResponse(user)), HttpStatus.OK);
     }
--- src/main/java/com/aegis/util/mail/MailUtil.java
+++ src/main/java/com/aegis/util/mail/MailUtil.java
@@ -18,20 +18,12 @@ import java.util.Properties;
 public class MailUtil {
     private final Logger logger = LoggerFactory.getLogger(MailUtil.class);
     private final JavaMailSender javaMailSender;
-    @Value("${spring.mail.host}")
-    private String host;
-    @Value("${spring.mail.port}")
-    private String port;
-    @Value("${spring.mail.username}")
-    private String user;
-    @Value("${spring.mail.password}")
-    private String password;
 
     public void send(EmailDetail details) {
         try {
             SimpleMailMessage mailMessage = new SimpleMailMessage();
 
-            mailMessage.setFrom("ahmadsafei.ridwan@gmail.com");
+            mailMessage.setFrom("safei.noreply@gmail.com");
             mailMessage.setTo(details.getRecipient());
             mailMessage.setText(details.getMsgBody());
             mailMessage.setSubject(details.getSubject());
@@ -39,40 +31,6 @@ public class MailUtil {
             // Sending the mail
             javaMailSender.send(mailMessage);
             logger.info("Success sending message!");
-        } catch (Exception e) {
-            logger.error("An error occurred, ", e);
-        }
-    }
-
-    public void sendMail(EmailDetail details) {
-        Properties props = new Properties();
-        props.put("mail.smtp.host", host);
-        props.put("mail.smtp.port", port); // for TLS
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        Authenticator auth = new Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(user, password);
-            }
-        };
-
-        Session session = Session.getInstance(props, auth);
-
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(user));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(details.getRecipient()));
-            message.setSubject(details.getSubject());
-            message.setText(details.getMsgBody());
-
-            Transport transport = session.getTransport("smtp");
-            transport.connect(host, user, password);
-            transport.sendMessage(message, message.getAllRecipients());
-            transport.close();
-
-            System.out.println("Email sent successfully!");
-
         } catch (Exception e) {
             logger.error("An error occurred, ", e);
         }
